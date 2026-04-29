f9bd543607f6ed5e5722b6f05317116c2fa23f95
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-29 20:25:13 +0530

    Init commit👍....

---
+++ src/main/java/com/activity_hub/notification_fee/TenantServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.activity_hub.notification_fee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/activity_hub/notification_fee/TenantServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activity_hub.notification_fee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
